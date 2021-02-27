8aa753a58ea57e3f947b10afd7239e998e204bef
eduardoMag <eduardobautista13@gmail.com>
2021-02-26 22:41:37 -0600

    adding prediction model script

--- PredictStockPrice-tensorFlow-keras/stock-prediction.py
+++ PredictStockPrice-tensorFlow-keras/stock-prediction.py
@@ -32,7 +32,7 @@ def load_data(ticker, n_steps=50, scale=True, shuffle=True, lookup_step=1, split
         # load it from yahoo_fin library
         df = si.get_data(ticker)
 
-    elif isinstance(tocker, pd.DataFrame):
+    elif isinstance(ticker, pd.DataFrame):
         # añready loaded, use it directly
         df = ticker
 
@@ -54,16 +54,20 @@ def load_data(ticker, n_steps=50, scale=True, shuffle=True, lookup_step=1, split
         df["date"] = df.index
     if scale:
         column_scaler = {}
+
         # scale the data (prices) from 0 to 1
         for column in feature_columns:
             scaler = preprocessing.MinMaxScaler()
             df[column] = scaler.fit_transform(np.expand_dims(df[column].values, axis=1))
             column_scaler[column] = scaler
+
         # add the MinMaxScaler instances to the result returned
         result["column_scaler"] = column_scaler
+
     # add the target column (label) by shifting by 'lookup_step'
     df['future'] = df['adjclose'].shift(-lookup_step)
     last_sequence = np.array(df[feature_columns].tails(lookup_step))
+
     # drop Nans
     df.dropna(inplace=True)
     sequence_data = []
@@ -72,3 +76,75 @@ def load_data(ticker, n_steps=50, scale=True, shuffle=True, lookup_step=1, split
         sequences.append(entry)
         if len(sequences) == n_steps:
             sequence_data.append([np.array(sequences), target])
+
+    last_sequence = list([s[:len(feature_columns)] for s in sequences]) + list(last_sequence)
+    last_sequence = np.array(last_sequence).astype(np.float32)
+
+    # add to result
+    result['last_sequence'] = last_sequence
+
+    # construct the X's and Y's
+    x, y = [], []
+    for seq, target in sequence_data:
+        x.append(seq)
+        y.append(target)
+
+    # convert to numpy arrays
+    x = np.array(x)
+    y = np.array(y)
+
+    if split_by_date:
+        # split the dataset into training & testing sets by date (not random)
+        train_samples = int((1 - test_size) * len(x))
+        result["x_train"] = x[:train_samples]
+        result["y_train"] = y[:train_samples]
+        result["x_test"] = x[train_samples:]
+        result["y_test"] = y[train_samples:]
+        if shuffle:
+            # shuffle the data sets for training
+            shuffle_in_unison(result["x_train"], result["y_train"])
+            shuffle_in_unison(result["x_test"], result["y_test"])
+    else:
+        # split the dataset randomly
+        result["x_train"], result["x_test"], result["y_train"], result["y_test"] = train_test_split(x, y,
+                                                                                                    test_size=test_size,
+                                                                                                    shuffle=shuffle)
+
+    # get the list of test set dates
+    dates = result["x_test"][:, -1, -1]
+    # retrieve test features from the original dataframe
+    result["test_df"] = result["df"].loc[dates]
+    # remove duplicated dates in the testing dataframe
+    result["test_df"] = result["test_df"][~result["test_df"].index.duplicated(keep='first')]
+    # remove dates from the training/testing sets & convert to float32
+    result["x_train"] = result["x_train"][:, :, :len(feature_columns)].astype(np.float32)
+    result["x_test"] = result["x_test"][:, :, :len(feature_columns)].astype(np.float32)
+
+    return result
+
+
+def create_model(sequence_length, n_features, units=256, cell=LSTM, n_layers=2, dropout=0.3, loss="mean_absolute_error",
+                 optimizer="rmsprop", bidirectional=False):
+    model = Sequential()
+    for i in range(n_layers):
+        if i == 0:
+            #first layer
+            if bidirectional:
+                model.add(Bidirectional(cell(units, return_sequences=True), batch_input_shape=(None, sequence_length, n_features)))
+            else: model.add(cell(units, return_sequences=True, batch_input_shape=(None, sequence_length, n_features)))
+        elif i == n_features - 1:
+            #last layer
+            if bidirectional:
+                model.add(Bidirectional(cell(units, return_sequences=False)))
+            else:
+                model.add(cell(units, return_sequences=False))
+        else:
+            #hidden layers
+            if bidirectional:
+                model.add(Bidirectional(cell(units, return_sequences=True)))
+            else:
+                model.add(cell(units, return_sequences=True))
+        #add dropout after each layer
+        model.add(Dense(1, activation="linear"))
+        model.compile(loss=loss, metrics["mean_absolute_error"], optimizer=optimizer)
+        return model
